4c28aa18c01c45f9a7830e85c74f0666e09dac37
victor calderon <calderon.j.victor@gmail.com>
2023-07-27 15:33:34 -0500

    Java Push One

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+//        sout create #5
+//        println prints out a line, print in the same line
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+        System.out.print("Yut");
+    }
+}
