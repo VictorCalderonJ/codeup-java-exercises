2068e866af08d3d01be58b34dc76219de054d7b3
victor calderon <calderon.j.victor@gmail.com>
2023-08-04 09:32:34 -0500

    test

---
+++ src/Bob.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nHi i'm Bob go ahead and ask me anything: ");
+        String userString = sc.nextLine();
+
+        if (userString.endsWith("?")) {
+            System.out.println("Sure.");
+        } else if (userString.endsWith("!")){
+            System.out.println("Whoa, chill out!");
+        } else if (userString.equals("")) {
+            System.out.println("Fine, be that way!");
+        } else {
+            System.out.println("whatever");;
+        }
+    }
+}
+
+
+
+
+
