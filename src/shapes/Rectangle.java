65b92c01f37fa2324ccd8492ca6a29d112bf6035
victor calderon <calderon.j.victor@gmail.com>
2023-08-08 11:38:34 -0500

    test

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,8 @@
+package shapes;
+
+public abstract class Rectangle extends Quadrilateral {
+
+    public Rectangle(double length, double width) {
+        super(length, width);
+    }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,7 @@
+package shapes;
+
+public abstract class Square extends Quadrilateral {
+    public Square(double length, double width) {
+        super(length, width);
+    }
+}
